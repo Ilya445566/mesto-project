d92a890362abb4ed44c3356977faad741bc058af
Ilya445566 <94865170+Ilya445566@users.noreply.github.com>
2022-03-26 21:43:02 +0300

    Update skript.js

--- skript.js
+++ skript.js
@@ -2,7 +2,7 @@ let main = document.querySelector('.main');
 let popup = document.querySelector('.popup');
 let redact = document.querySelector('.profile__redact')
 let closePopup = document.querySelector('.popup__close-img')
-let popupMesto = document.querySelector('.popup-mesto') 
+let popupMesto = document.querySelector('.popup-mesto')
 let closeButtonMesto = document.querySelector('.popup-mesto__close-img')
 const popupTitle = document.querySelector('.popup__title')
 const popupContainer = document.querySelector('.popup__container')
@@ -15,7 +15,6 @@ const saveButtonMesto = document.querySelector('.popup-mesto__save-button')
 const profileName = document.querySelector('.profile__title')
 const profileAbout = document.querySelector('.profile__subtitle')
 const popupImgImage = document.querySelector('.popup-img__image')
-const elementImg = document.querySelector('.elements__img')
 const popupImage = document.querySelector('.popup-img')
 const elements = document.querySelector('.elements')
 const element = elements.querySelector('.elements__element')
@@ -28,28 +27,28 @@ let form = document.querySelector('.popup__form')
 
 const initialCards = [
   {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
   },
   {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
   },
   {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
   },
   {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
   },
   {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
   },
   {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
 
@@ -60,11 +59,11 @@ for (let heart of hearts) {
 }
 
 function popupOpen(popup) {
-    popup.classList.add('popup_opened');
+  popup.classList.add('popup_opened');
 };
 
 function popupClose() {
-    popup.classList.remove('popup_opened');
+  popup.classList.remove('popup_opened');
 };
 
 function popupMestoClose() {
@@ -83,6 +82,7 @@ function addElement(elementName, elementLink) {
   const elementTemplate = document.querySelector('#element-template').content;
   const elementElement = elementTemplate.querySelector('.elements__element').cloneNode(true);
   let hearts = document.getElementsByClassName("elements__heart-icon");
+  const elementImg = document.querySelector('.popup-img');
 
   elementElement.querySelector('.elements__title').textContent = elementName;
   elementElement.querySelector('.elements__img').src = elementLink;
@@ -90,15 +90,14 @@ function addElement(elementName, elementLink) {
 
   for (let heart of hearts) {
     heart.addEventListener("click", () => heart.classList.toggle('elements__heart-icon_active'));
-  }
+  };
 
-  elementImg.addEventListener('click', () => {
-    openPopupImage(elementLink, elementName);
+  elementImg.addEventListener('click', () => { openPopupImage(elementLink, elementName)
+  });
 };
 
 
-
-function editProfileInfo() { 
+function editProfileInfo() {
   profileName.textContent = nameInput.value;
   profileAbout.textContent = aboutInput.value;
 
@@ -112,7 +111,7 @@ function openPopupImage(imgLink, imgTitle) {
 
   popupOpen(popupImage);
 };
-  
+
 
 function addNewElement() {
   const newName = document.querySelector('.popup-mesto__input_first');
@@ -129,7 +128,7 @@ function addNewElement() {
 
 
 saveButtonMesto.addEventListener('click', addNewElement)
-  
+
 
 closePopup.addEventListener('click', popupClose);
 
