2303a6e5b8cf97635ed5857e74e120903d84d4e4
Ilya445566 <94865170+Ilya445566@users.noreply.github.com>
2022-07-02 20:35:14 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -20,10 +20,7 @@ const editButton = document.querySelector('.profile__edit-button')
 const editform = document.querySelector('.popup__form')
 const mestoForm = document.querySelector('.popup-mesto__form')
 const elementTemplate = document.querySelector('#element-template').content;
- 
 const initialCards = [
- 
-
   {
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -34,11 +31,12 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
   },
  
+
   {
     name: 'Иваново',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
   },
- 
+
   {
     name: 'Камчатка',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
@@ -55,28 +53,30 @@ const initialCards = [
   }
 ];
  
+  heart.addEventListener("click", () => heart.classList.toggle('elements__heart-icon_active'));
+ 
+
 function openPopup(popup) {
   popup.classList.add('popup_opened');
 };
+ 
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
+};
 
-};
-function likePicture(item) {
-  item.classList.toggle('elements__heart-icon_active');
-};
 function addElement(elementName, elementLink) {
   const elementElement = elementTemplate.querySelector('.elements__element').cloneNode(true);
-  const heart = elementElement.querySelectorAll("elements__heart-icon");
   const elementImg = elementElement.querySelector('.elements__img');
   elementElement.querySelector('.elements__title').textContent = elementName;
   elementElement.querySelector('.elements__img').src = elementLink;
   elementElement.querySelector('.elements__img').alt = elementName;
-  elements.prepend(elementElement);
-    heart.addEventListener("click", () => {
-      likePicture(heart)
-    });
 
+   elementElement.querySelector(".elements__heart-icon").addEventListener("click", activateElement);
+ 
+
+  function activateElement(evt) {
+    evt.target.classList.toggle("elements__heart-icon_active");
+  };
   elementImg.addEventListener('click', () => {
     openPopupImage(elementLink, elementName)
   });
@@ -91,21 +91,25 @@ function editProfileInfo(evt) {
   profileAbout.textContent = aboutInput.value;
   closePopup(popupEdit);
 };
+ 
 function openPopupImage(imgLink, imgTitle) {
   openPopup(popupImg);
+ 
   popupImgImage.setAttribute('src', imgLink);
   popupImageTitle.textContent = imgTitle;
 };
+
 function createCard(evt) {
   evt.preventDefault();
   const newName = document.querySelector('.popup-mesto__input_first');
   const newLink = document.querySelector('.popup-mesto__input_second');
+
   addElement(newName.value, newLink.value);
   newName.value = '';
   newLink.value = '';
+ 
   closePopup(popupMesto);
 }
-
 mestoForm.addEventListener('submit', createCard);
 editform.addEventListener('submit', editProfileInfo);
 redact.addEventListener('click', () => {openPopup(popupEdit)});
@@ -114,3 +118,4 @@ editButton.addEventListener('click', () => {openPopup(popupMesto)});
 closeButtonMesto.addEventListener('click', () => {closePopup(popupMesto)});
 closeButtonImg.addEventListener('click', () => {closePopup(popupImg)});
 initialCards.forEach(item => addElement(item.name, item.link));
+ 
