bbc740565785845e0ccf0b91f4644778b18830f7
Ilya445566 <94865170+Ilya445566@users.noreply.github.com>
2022-07-04 21:22:14 +0300

    Update script.js
    Signed-off-by: Ilya445566 <94865170+Ilya445566@users.noreply.github.com>

--- scripts/script.js
+++ scripts/script.js
@@ -3,21 +3,21 @@ const popupEdit = document.querySelector('.popup-edit');
 const redact = document.querySelector('.profile__redact')
 const profileCloseBtn = document.querySelector('.popup__close-img')
 const popupMesto = document.querySelector('.popup-mesto')
-const closeButtonMesto = document.querySelector('.popup-mesto__close-img')
+const mestoBtnclose = document.querySelector('.popup-mesto__close-img')
 const popupTitle = document.querySelector('.popup__title')
 const popupContainer = document.querySelector('.popup__container')
 const popupImageTitle = document.querySelector('.popup-img__title')
 const popupImg = document.querySelector('.popup-img')
 const nameInput = document.querySelector('.popup__input_name');
 const aboutInput = document.querySelector('.popup__input_description');
-const closeButtonImg = document.querySelector('.popup-img__close-img')
+const imgButtonclose = document.querySelector('.popup-img__close-img')
 const profileName = document.querySelector('.profile__title')
 const profileAbout = document.querySelector('.profile__subtitle')
 const popupImgImage = document.querySelector('.popup-img__image')
 const popupImage = document.querySelector('.popup-img')
 const elements = document.querySelector('.elements')
-const editButton = document.querySelector('.profile__edit-button')
-const editform = document.querySelector('.popup__form')
+const buttonEdit = document.querySelector('.profile__edit-button')
+const formEdit = document.querySelector('.popup__form')
 const mestoForm = document.querySelector('.popup-mesto__form')
 const elementTemplate = document.querySelector('#element-template').content;
 const newName = document.querySelector('.popup-mesto__input_name');
@@ -74,7 +74,6 @@ function closePopup(popup) {
 function addElement(elementName, elementLink) {
 
   const elementElement = elementTemplate.querySelector('.elements__element').cloneNode(true);
-  const hearts = elementElement.getElementsByClassName("elements__heart-icon");
   const elementImg = elementElement.querySelector('.elements__img');
   
   elementElement.querySelector('.elements__title').textContent = elementName;
@@ -131,11 +130,9 @@ mestoForm.reset();
 }
 
 mestoForm.addEventListener('submit', createCard);
-editform.addEventListener('submit', editProfileInfo);
+formEdit.addEventListener('submit', editProfileInfo);
 redact.addEventListener('click', () => {openPopup(popupEdit)});
 profileCloseBtn.addEventListener('click', () => {closePopup(popupEdit)});
-editButton.addEventListener('click', () => {openPopup(popupMesto)});
-closeButtonMesto.addEventListener('click', () => {closePopup(popupMesto)});
-closeButtonImg.addEventListener('click', () => {closePopup(popupImg)});
-initialCards.forEach(item => addElement(item.name, item.link));
- 
+buttonEdit.addEventListener('click', () => {openPopup(popupMesto)});
+mestoBtnclose.addEventListener('click', () => {closePopup(popupMesto)});
+imgButtonclose.addEventListener('click', () => {closePopup(popupImg)});
